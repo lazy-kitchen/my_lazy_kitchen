494fb75814bb00c6c204a2ae13cad38296ffe16b
mc962 <11969409+mc962@users.noreply.github.com>
2020-07-16 01:11:21 -0400

    Add beginning styling to home page.

--- src/client/views/landings/Home.vue
+++ src/client/views/landings/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <main id="root">
     <h1>Step into My Lazy Kitchen</h1>
-    <form action="/search" method="post">
-      <label for="search">Search Recipes</label>
-      <input type="search" id="search" v-model.trim="query" placeholder="Search recipes..." />
+    <form id="home_search_form" action="/search" method="post">
+      <label for="search" class="visually-hidden">Search Recipes</label>
+      <div class="search-controls-container">
+        <input type="search" id="search" v-model.trim="query" placeholder="Search recipes..." />
 
-      <input type="submit" value="Submit" disabled="disabled" />
+        <input type="submit" class="submit-search-btn" value="Find" disabled="disabled" />
+      </div>
     </form>
   </main>
 </template>
@@ -24,3 +26,40 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style scoped lang="scss">
+  #root {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #home_search_form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .search-controls-container {
+      display: flex;
+      flex-direction: row;
+      width: 35%;
+      .submit-search-btn {
+        height: 100%;
+        cursor: pointer;
+        margin: auto 2%;
+        padding: 2%;
+      }
+
+      #search{
+        margin: 0 2%;
+        width: 100%;
+      }
+    }
+  }
+
+  .visually-hidden {
+    position: absolute !important;
+    height: 1px; width: 1px;
+    overflow: hidden;
+    clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+    clip: rect(1px, 1px, 1px, 1px);
+  }
+</style>
