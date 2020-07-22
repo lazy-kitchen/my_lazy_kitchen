b460a59acfeaeab00bc9f3ea11abb60a0182eab8
mc962 <11969409+mc962@users.noreply.github.com>
2020-07-22 00:01:06 -0400

    Update page title

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,13 @@ module.exports = {
       app: path.join(__dirname, 'src', 'client', 'main.ts')
     }
   },
+  chainWebpack: config => {
+    config.plugin('html')
+        .tap(args => {
+          args[0].title = 'Lazy Kitchen';
+          return args;
+        })
+  },
   pwa: {
     name: 'Lazy Kitchen'
   }
