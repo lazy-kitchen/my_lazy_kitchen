8f665c825c0295b55f37af9af64be8af5a4ab1a4
mc962 <11969409+mc962@users.noreply.github.com>
2020-07-28 22:56:46 -0400

    Add draggable list ability

--- src/client/components/recipes/recipe_steps/Steps.vue
+++ src/client/components/recipes/recipe_steps/Steps.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
-        <ul id="recipe_steps" ref="recipeStepsList">
-            <draggable v-model="this.steps" group="steps" @start="drag=true" @stop="drag=false">
-                <recipe-step v-for="step in this.steps" :key="step.id" v-bind:recipe-step="step" />
-            </draggable>
-        </ul>
+        <draggable tag="ul"
+                   id="recipe_steps"
+                   ref="recipeStepsList"
+                   group="steps"
+                   @start="drag=true"
+                   @stop="drag=false"
+                   v-model="steps" >
+            <recipe-step v-for="step in steps" :key="step.id" v-bind:recipe-step="step" />
+        </draggable>
         <button id="add_recipe_step_btn" class="add-step" type="button" @click="addStep">Add Step</button>
     </div>
 </template>
@@ -13,17 +17,30 @@
     import Vue from 'vue';
     import RecipeStep from '@/client/components/recipes/recipe_steps/RecipeStep.vue';
     import store from "@/client/store";
-    import {mapMutations, mapState} from "vuex";
-    import {ADD_RECIPE_STEP, RECIPE_STEPS_NAMESPACE} from "@/client/store/modules/forms/recipe_steps";
+    import {mapMutations} from "vuex";
+    import {
+        ADD_RECIPE_STEP,
+        RECIPE_STEPS_NAMESPACE,
+        UPDATE_RECIPE_STEPS
+    } from "@/client/store/modules/forms/recipe_steps";
     import draggable from 'vuedraggable'
+    import Step from '@/server/db/models/recipe_step';
+
 
     export default Vue.extend({
         name: "steps",
         components: {RecipeStep, draggable},
         computed: {
-            ...mapState(RECIPE_STEPS_NAMESPACE, {
-                steps: 'steps',
-            })
+            steps: {
+                get(): Array<Step> {
+                    return this.$store.state.recipes.recipeForm.recipe.steps;
+                },
+                set(steps: Array<Step>) {
+                    this.$store.commit(`${RECIPE_STEPS_NAMESPACE}/${UPDATE_RECIPE_STEPS}`, {
+                        steps: steps
+                    });
+                }
+            }
         },
         methods: {
             ...mapMutations(RECIPE_STEPS_NAMESPACE, [
--- src/client/store/modules/forms/recipe_steps.ts
+++ src/client/store/modules/forms/recipe_steps.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import RecipeStep from "@/server/db/models/recipe_step";
-import recipe from "@/server/db/models/recipe";
 
 export const RECIPE_STEPS_NAMESPACE = 'recipes/recipeForm/recipe';
 
@@ -20,17 +19,20 @@ const RecipeSteps = {
     }),
     mutations: {
         // meant to load initial recipe steps state, formatted, from recipe
-        addRecipeSteps(state: any, recipeSteps: Array<RecipeStep>) {
+        addRecipeSteps(state: RecipeStepsState, recipeSteps: Array<RecipeStep>) {
             Vue.set(state, 'steps', recipeSteps);
         },
         // meant to add new recipe step from form, basically pre-reserve space
-        addRecipeStep(state: any, recipeStepId: number) {
+        addRecipeStep(state: RecipeStepsState, recipeStepId: number) {
             const newStep = {
                 id: recipeStepId,
-                action: StepAction.Create
-            };
+                action: StepAction.Create,
+                stepNumber: -1,
+                instruction: '',
+                createdAt: null,
+                updatedAt: null
+            } as unknown as RecipeStep;
 
-            // Vue.set(state.steps, recipeStepId, newStep);
             state.steps.push(newStep);
         },
         // meant to remove recipe step from form
@@ -58,11 +60,44 @@ const RecipeSteps = {
             }
         },
         undoRemoveRecipeStep(state: RecipeStepsState, {recipeStepId}: { recipeStepId: number}) {
+            const currentStepStatePosition: number = state.steps.findIndex((step) => {
+                return step.id === recipeStepId;
+            });
+
+            if (currentStepStatePosition < 0) {
+                console.error(`Recipe step not found with identifier: ${recipeStepId}`);
+                return;
+            }
+
+            const newStepState = state.steps[currentStepStatePosition];
             // While not ideal, assume that if step has been touched, then it should be considered 'updated' even with the undo,
             // as it is difficult to tell if things have changed
-            Vue.set(state.steps[recipeStepId], 'action', StepAction.Update)
+            Vue.set(newStepState, 'action', StepAction.Update)
+
+            state.steps.splice(currentStepStatePosition, 1, newStepState);
         },
-        updateRecipeStep(state: RecipeStepsState, {recipeStepId, property, value}: {recipeStepId: number; property: string; value: any}) {
+        updateRecipeSteps(state: RecipeStepsState, {steps}: {steps: Array<RecipeStep>} ) {
+            const sortedUpdateSteps = steps.map((step, idx) => {
+                const stepNumber = idx + 1;
+
+                let stepAction;
+                if (step.action !== StepAction.Update) {
+                    stepAction = step.action;
+                } else {
+                    stepAction = StepAction.Update;
+                }
+
+                return {
+                    ...step,
+                    stepNumber: stepNumber,
+                    // If list is reorganized, then many step actions may be reorganized, and therefor all steps are marked as 'updated'
+                    action: stepAction
+                };
+            });
+            Vue.set(state, 'steps', sortedUpdateSteps);
+        },
+
+        updateRecipeStep(state: RecipeStepsState, {recipeStepId, property, value}: {recipeStepId: number; property: string; value: string | boolean | number}) {
             const currentStepStatePosition: number = state.steps.findIndex((step) => {
                 return step.id === recipeStepId;
             });
@@ -94,6 +129,7 @@ const RecipeSteps = {
 export const ADD_RECIPE_STEPS = 'addRecipeSteps';
 export const ADD_RECIPE_STEP = 'addRecipeStep';
 export const UPDATE_RECIPE_STEP = 'updateRecipeStep';
+export const UPDATE_RECIPE_STEPS = 'updateRecipeSteps';
 export const REMOVE_RECIPE_STEP = 'removeRecipeStep';
 export const UNDO_REMOVE_RECIPE_STEP = 'undoRemoveRecipeStep';
 
